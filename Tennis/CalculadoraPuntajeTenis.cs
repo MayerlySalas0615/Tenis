ae976745f70a36098a5fd3649b681b8eacd2b4d6
MayerlySalas0615 <mayerly.salas@sinco.com.co>
2025-10-28 11:49:33 -0500

    🟥 Inicio de puntaje de Tenis

---
+++ Tennis/CalculadoraPuntajeTenis.cs
@@ -0,0 +1,24 @@
+﻿namespace Tennis;
+
+public class CalculadoraPuntajeTenis
+{
+    [Fact]
+    public void Jugador1_1P_Jugador2_0P()
+    {
+        //Arrange
+        int Jugador1 = 15;
+        int Jugador2 = 0;
+        
+        //Act
+        var resultado = puntaje(Jugador1, Jugador2);
+        
+        //Assert
+        Assert.Equal(Jugador1, resultado);
+
+    }
+
+    private int puntaje(int jugador1, int jugador2)
+    {
+        throw new NotImplementedException();
+    }
+}
